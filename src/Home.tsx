24979d1f795a88725741f4b9cc898684affe1ec1
Aditya Shibu <as2397@hw.ac.uk>
2024-01-01 20:23:51 +0400

    Added function for displaying the home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,28 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+
+
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <p>This is the home page.</p>
+            <Container maxWidth="xl" sx={{ mt: 8 }}>
+                <Typography
+                    variant="h4"
+                    align="center"
+                    sx={{ fontFamily: 'monospace', fontWeight: 700, mb: 4 }}
+                >
+                    Welcome to BSC Year 2 Semester 2 Portal
+                </Typography>
+                <Typography variant="body1" align="center" sx={{ mb: 4 }}>
+                    Explore teaching timetables, resources, and more!
+                </Typography>
+                <Typography variant="body1" align="center">
+                    <Link to="/timetables" style={{ textDecoration: 'none' }}>
+                        View Teaching Timetables
+                    </Link>
+                </Typography>
+            </Container>
         </div>
     );
 }
